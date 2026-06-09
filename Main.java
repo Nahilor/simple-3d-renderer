2f9f346315586fd72c120d017f3981f4fde4b89e
Nahilor <nahomambachew16@gmail.com>
2026-06-09 03:46:40 -0700

    Finished barycentric/raterization

--- Main.java
+++ Main.java
@@ -10,12 +10,14 @@
                     https://youtu.be/vzt9c7iWPxs?si=TOQ86q2IAes74p_J Matrix multiplication
                     https://youtu.be/EZufiIwwqFA?si=_fYYpR8_IWPzAlHT Rotation
                     https://youtu.be/BKsZrkI6sro?si=hqzYkazhZAoiHXHJ 3d rotation
+                    https://en.wikipedia.org/wiki/Barycentric_coordinate_system Barycentric coordinate system
 
 */
 
 import javax.swing.*;
 import java.awt.*;
 import java.awt.geom.Path2D;
+import java.awt.image.BufferedImage;
 import java.util.List;
 import java.util.ArrayList;
 
@@ -66,8 +68,6 @@ public class Main {
                 g2.setColor(Color.BLACK);
                 g2.fillRect(0, 0, getWidth(), getHeight());
 
-                
-
 
                 // rotation
                 double horizontalRotationRadian = Math.toRadians(horizontaSlider.getValue());
@@ -87,23 +87,86 @@ public class Main {
 
                 Matrix3 transform = rotationMatrix3X.multiply(rotationMatrix3Y);
                 // this is ignoring the z axis
-                g2.translate(getWidth() / 2, getHeight() / 2); // Moves the origin (0,0) point to the center of the panel
+                // WireFrame rendering
+                // g2.translate(getWidth() / 2, getHeight() / 2); // Moves the origin (0,0) point to the center of the panel
                 // This makes the 3D shape render from the middle of the window instead of the top-left corner
-                g2.setColor(Color.WHITE);
+                // g2.setColor(Color.WHITE);
+                // for (Triangle t : tetrahedron) {
+                //     Vertex v1 = transform.transform(t.v1);
+                //     Vertex v2 = transform.transform(t.v2);
+                //     Vertex v3 = transform.transform(t.v3);
+                //     Path2D path = new Path2D.Double();
+                //     path.moveTo(v1.x, v1.y); // moves the pen to the specified point
+                //     path.lineTo(v2.x, v2.y); // draws a line from vertex1(Point1) to vertex2(Point2)
+                //     path.lineTo(v3.x, v3.y); // same from the current location of the pen to the specified point
+                //     path.closePath(); // this automatically closes the path back the first vertex
+                //     g2.draw(path); // This renders the lines. Here there is no z axis it is just orthogonal view
+                // }
+
+                BufferedImage img = new BufferedImage(getWidth(), getHeight(), BufferedImage.TYPE_INT_ARGB);
+                double[] zBuffer = new double[img.getWidth() * img.getHeight()];
+
+                for (int q = 0; q < zBuffer.length; q++) {
+                    zBuffer[q] = Double.NEGATIVE_INFINITY;
+                }
+
                 for (Triangle t : tetrahedron) {
                     Vertex v1 = transform.transform(t.v1);
                     Vertex v2 = transform.transform(t.v2);
                     Vertex v3 = transform.transform(t.v3);
-                    // v1 = rotationMatrix3X.transform(t.v1);
-                    // v2 = rotationMatrix3X.transform(t.v2);
-                    // v3 = rotationMatrix3X.transform(t.v3);
-                    Path2D path = new Path2D.Double();
-                    path.moveTo(v1.x, v1.y); // moves the pen to the specified point
-                    path.lineTo(v2.x, v2.y); // draws a line from vertex1(Point1) to vertex2(Point2)
-                    path.lineTo(v3.x, v3.y); // same from the current location of the pen to the specified point
-                    path.closePath(); // this automatically closes the path back the first vertex
-                    g2.draw(path); // This renders the lines. Here there is no z axis it is just orthogonal view
+
+                    // since we are not using Graphics2D anymore,
+                    // we have to do translation manually
+                    v1.x += getWidth() / 2;
+                    v1.y += getHeight() / 2;
+                    v2.x += getWidth() / 2;
+                    v2.y += getHeight() / 2;
+                    v3.x += getWidth() / 2;
+                    v3.y += getHeight() / 2;
+
+                    // compute rectangular bounds for triangle
+                    int minX = (int) Math.max(0, Math.ceil(Math.min(v1.x, Math.min(v2.x, v3.x))));
+                    int maxX = (int) Math.min(img.getWidth() - 1, 
+                                            Math.floor(Math.max(v1.x, Math.max(v2.x, v3.x))));
+                    int minY = (int) Math.max(0, Math.ceil(Math.min(v1.y, Math.min(v2.y, v3.y))));
+                    int maxY = (int) Math.min(img.getHeight() - 1,
+                                            Math.floor(Math.max(v1.y, Math.max(v2.y, v3.y))));
+
+                    double triangleArea =
+                    (v1.y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - v1.x);
+
+                    for (int y = minY; y <= maxY; y++) {
+                        for (int x = minX; x <= maxX; x++) {
+                            double b1 = 
+                            ((y - v3.y) * (v2.x - v3.x) + (v2.y - v3.y) * (v3.x - x)) / triangleArea;
+                            double b2 =
+                            ((y - v1.y) * (v3.x - v1.x) + (v3.y - v1.y) * (v1.x - x)) / triangleArea;
+                            double b3 =
+                            ((y - v2.y) * (v1.x - v2.x) + (v1.y - v2.y) * (v2.x - x)) / triangleArea;
+                            if (b1 >= 0 && b1 <= 1 &&
+                                b2 >= 0 && b2 <= 1 &&
+                                b3 >= 0 && b3 <= 1) {
+
+                                double depth =
+                                    b1 * v1.z +
+                                    b2 * v2.z +
+                                    b3 * v3.z;
+
+                                int zIndex = y * img.getWidth() + x;
+
+                                if (depth > zBuffer[zIndex]) {
+                                    zBuffer[zIndex] = depth;
+                                    img.setRGB(x, y, t.color.getRGB());
+                                }
+                            }
+                            
+                        }
+                    }
+
+
                 }
+
+                g2.drawImage(img, 0, 0, null);
             }
         };
         pane.add(renderViewPanel, BorderLayout.CENTER);
